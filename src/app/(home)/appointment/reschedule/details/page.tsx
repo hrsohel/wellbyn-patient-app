67d5db89079d1332e8b8db4122442f2c5aae0f65
fahim <fahimahamedweb@gmailo.com|>
2025-08-02 03:11:21 +0600

    done some issues

--- src/app/(home)/appointment/reschedule/details/page.tsx
+++ src/app/(home)/appointment/reschedule/details/page.tsx
@@ -3,15 +3,33 @@
 import { useState } from "react"
 import { Calendar, Clock, MapPin, FileText, MoreHorizontal } from "lucide-react"
 import Link from "next/link"
+import CancelAppointmentModal from "./CancelAppointmentModal"
 export default function PatientAppointment() {
   const [showTimer, setShowTimer] = useState(false)
  const [activeTab, setActiveTab] = useState("doctor");
 
 
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const handleConfirmCancel = () => {
+    console.log("Appointment cancelled")
+    setIsModalOpen(false)
+    // Add your cancellation logic here
+  }
 
+  const handleReschedule = () => {
+    console.log("Redirecting to reschedule")
+    setIsModalOpen(false)
+    // Add your reschedule logic here
+  }
   return (
 
 <div className="relative">
+  <CancelAppointmentModal
+        open={isModalOpen}
+        onOpenChange={setIsModalOpen}
+        onConfirmCancel={handleConfirmCancel}
+        onReschedule={handleReschedule}
+      />
     <div className="flex gap-1 p-3 mb-8 bg-white w-md rounded-lg ">
           <button
             onClick={() => setActiveTab("doctor")}
@@ -90,14 +108,7 @@ export default function PatientAppointment() {
               ✓ Complete
             </span>
           </div>
-          <button className="w-full flex items-center justify-center gap-2 rounded-md shadow-sm bg-[#FBF7EB] px-4 py-2 text-sm font-medium text-[#93531F] hover:bg-orange-100">
-          <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M14.1177 21.367C13.6841 21.773 13.1044 22 12.5011 22C11.8978 22 11.3182 21.773 10.8845 21.367C6.91302 17.626 1.59076 13.4469 4.18627 7.37966C5.58963 4.09916 8.95834 2 12.5011 2C16.0439 2 19.4126 4.09916 20.816 7.37966C23.4082 13.4393 18.099 17.6389 14.1177 21.367Z" stroke="#93531F" stroke-width="1.5"/>
-<path d="M9.5 11.8333C9.5 11.8333 10.375 11.8333 11.25 13.5C11.25 13.5 14.0294 9.33333 16.5 8.5" stroke="#93531F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-
-            Check In
-          </button>
+         
         </div>
 
         {/* Visit Reason */}
@@ -204,81 +215,33 @@ export default function PatientAppointment() {
         </div>
 
         {/* Start Appointment Button */}
-        <button
-          className="w-full bg-[#2E8BC9] hover:bg-blue-600 text-white py-2 text-base font-medium rounded-md"
+        <div className="flex gap-3 items-center">
+            <button
+          className="flex gap-2 items-center shadow-sm bg-[#EEFEE7] hover:bg-blue-600 text-[#2E8BC9] px-4 py-2 text-base font-medium rounded-md"
           onClick={() => setShowTimer(true)}
-        >
-          Start Appointment
-        </button>
-      </div>
-
-      {/* Timer Overlay */}
-      {showTimer && (
-        <div className=" inset-0  bg-[#F2F8FD] flex items-center justify-center z-50">
-          <div className="bg-[#F2F8FD] shadow-md absolute left-[450px] bottom-[-85px] rounded-lg p-6 mx-4 w-full max-w-sm">
-            {/* Patient Info in Timer */}
-            <div className="flex items-center gap-3 mb-6">
-              <div className="w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center">
-                <span className="text-white font-semibold">Ma</span>
-              </div>
-              <div className="flex-1">
-                <h3 className="font-semibold text-gray-900">Mahmudur Rahman</h3>
-                <p className="text-sm text-gray-500">Patient ID: P607RB</p>
-              </div>
-              <button className="p-2 text-blue-500 rounded-full hover:bg-gray-100">
-               <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_3423_46603)">
-<rect x="4" y="1" width="40" height="40" rx="8" fill="#2E8BC9"/>
-<path d="M24 11C29.5221 11 33.9998 15.2834 34 20.5664C34 25.8496 29.5222 30.1338 24 30.1338C23.3507 30.1347 22.7032 30.0738 22.0654 29.9541C21.6066 29.8679 21.377 29.8252 21.2168 29.8496C21.0565 29.8741 20.8292 29.9947 20.375 30.2363C19.0902 30.9196 17.5922 31.1605 16.1514 30.8926C16.6988 30.2191 17.0723 29.4111 17.2373 28.5449C17.3373 28.0149 17.0899 27.4999 16.7188 27.123C15.0332 25.4115 14 23.1048 14 20.5664C14.0002 15.2834 18.4779 11 24 11ZM20 20C19.4477 20 19 20.4477 19 21C19 21.5523 19.4477 22 20 22H20.0088C20.5611 22 21.0088 21.5523 21.0088 21C21.0088 20.4477 20.5611 20 20.0088 20H20ZM23.9951 20C23.443 20.0002 22.9951 20.4478 22.9951 21C22.9951 21.5522 23.443 21.9998 23.9951 22H24.0049L24.1064 21.9951C24.6109 21.9441 25.0049 21.5179 25.0049 21C25.0049 20.4821 24.6109 20.0559 24.1064 20.0049L24.0049 20H23.9951ZM27.9912 20C27.4389 20 26.9912 20.4477 26.9912 21C26.9912 21.5523 27.4389 22 27.9912 22H28C28.5523 22 29 21.5523 29 21C29 20.4477 28.5523 20 28 20H27.9912Z" fill="white"/>
-</g>
-<defs>
-<filter id="filter0_d_3423_46603" x="0" y="0" width="48" height="48" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="3"/>
-<feGaussianBlur stdDeviation="2"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0.101961 0 0 0 0 0.25098 0 0 0 0 0.376471 0 0 0 0.1 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_3423_46603"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_3423_46603" result="shape"/>
-</filter>
-</defs>
+        ><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.3178 13C12.9345 11.9251 14 10.087 14 8C14 4.68629 11.3137 2 8 2C7.54173 2 7.09547 2.05137 6.66667 2.14868M11.3178 13V10.6667M11.3178 13H13.6667M4.66667 3.01037C3.05869 4.08671 2 5.91972 2 8C2 11.3137 4.68629 14 8 14C8.45827 14 8.90453 13.9486 9.33333 13.8513M4.66667 3.01037V5.33333M4.66667 3.01037H2.33333" stroke="#2E8BC9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 
-              </button>
-            </div>
+          Reschedule
+        </button>
+<div className="terxt-gray-800">
+   OR
+</div>
+   
+             <button
+          className="flex gap-2 items-center shadow-sm bg-white hover:bg-blue-600 text-[#B42121] px-4 py-2 text-base font-medium rounded-md"
+          onClick={() => setIsModalOpen(true)}
+        >
 
-            {/* Timer Display */}
-            <div className="bg-gray-900 rounded-lg p-6 text-center mb-4">
-              <div className="text-white text-3xl font-mono font-bold mb-2">00 : 30 : 56</div>
-              <div className="flex justify-center gap-8 text-xs text-gray-400">
-                <span>hours</span>
-                <span>minute</span>
-                <span>second</span>
-              </div>
-            </div>
-
-            {/* Timer Controls */}
-            <div className="flex gap-3">
-              <button 
-                className="flex-1 rounded-md border border-[#2E8BC9] bg-white text-[#2E8BC9] py-2 px-4 text-sm font-medium hover:bg-gray-50"
-                onClick={() => setShowTimer(false)}
-              >
-                View Details
-              </button>
-              <Link className="flex-1 " href="/appointment/prescription">
-               <button
-                className="rounded-md bg-red-500 w-full py-2 px-4 text-sm font-medium text-white hover:bg-red-600"
-                  
-              >
-                Finish
-              </button>
-              </Link>
-             
-            </div>
-          </div>
+          Cancel
+        </button>
         </div>
-      )}
+      
+    
+        
+      </div>
+
     </div>
 </div>
 
